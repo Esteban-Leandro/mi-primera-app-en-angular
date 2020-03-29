5fca9dc73b1b3c423dc789356e618a2620b5fe6a
Esteban-Leandro <esteban.vilchesf@gmail.com>
2020-03-28 21:51:02 -0300

    Proyecto conectado con firebase -Equipo -Productos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FooterComponent } from './shared/footer/footer.component';
+import { PortafolioComponent } from './pages/portafolio/portafolio.component';
+import { AboutComponent } from './pages/about/about.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    PortafolioComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/services/productos.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Producto } from '../interfaces/producto.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductosService {
+
+  productos: Producto [] = [];
+  constructor(private http: HttpClient) {
+    this.cargarProducto();
+  }
+
+  private cargarProducto() {
+    this.http.get('https://angular-html-8ca6d.firebaseio.com/productos_idx.json')
+    .subscribe((resp: Producto[]) => {
+      this.productos = resp;
+    });
+  }
+}
